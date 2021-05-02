95dd8da39013867834ce8f9749c1309cd9cec273
bashman1 <wamulabash1@gmail.com>
2021-05-02 21:45:29 +0300

    broght products to home page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,24 +24,19 @@ class UserController extends Controller
 
       foreach($categories as $cat){
          $subCategory = DB::select('SELECT * FROM sub_categories WHERE category_id = "'.$cat->id.'" ');
-         // $cat->sub_category = $subCategory;
-         // array_push($category, $cat);
-         // $temp_sub_cat = [];
          $temp_sub_cat = [];
          foreach($subCategory as $sub_cat){
             $products = DB::select('SELECT* FROM products WHERE sub_category_id = "'.$sub_cat->id.'"');
             $sub_cat->products = $products;
             array_push($temp_sub_cat, $sub_cat);
-            // $sub_cat->products = $products;
-            // $cat->sub_category = $sub_cat;
-            // array_push($category, $cat);
          }
+
 
          $cat->sub_category = $temp_sub_cat;
          array_push($category, $cat);
       }
-      // return $category;
-      return view('welcome')->with('category', $category);
+      $category_products = DB::select('SELECT p.*, c.name  AS category FROM products p INNER JOIN categories c ON p.category_id = c.id');
+      return view('welcome')->with('category', $category)->with('products', $category_products);
    }
 
     //Show login form
